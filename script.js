27ddbe9aff9f7c166db279edd413aabfb9fbaf0f
Brandon Walker <brandonwalker.861@gmail.com>
2022-05-22 08:35:29 -0400

    functional rps-ui, needs refactor

--- script.js
+++ script.js
@@ -5,9 +5,7 @@
 //   (computerSelection === 'paper' && playerSelection === 'rock') ||
 //   (computerSelection === 'scissors' && playerSelection === 'paper');
 
-let playerScore = 0;
-let computerScore = 0;
-
+// Computer choice
 function computerPlay() {
   let random = Math.trunc(Math.random() * 3) + 1;
   if (random === 1) {
@@ -16,47 +14,6 @@ function computerPlay() {
     return 'paper';
   } else if (random === 3) {
     return 'scissors';
-  }
-}
-
-function playRound(playerSelection, computerSelection) {
-  playerSelection = prompt('Choose rock, paper, or scissors');
-  playerSelection.toLowerCase();
-  computerSelection = computerPlay();
-  console.log(playerSelection, computerSelection);
-
-  if (
-    (computerSelection === 'rock' && playerSelection === 'scissors') ||
-    (computerSelection === 'paper' && playerSelection === 'rock') ||
-    (computerSelection === 'scissors' && playerSelection === 'paper')
-  ) {
-    console.log(
-      `You lose! ${
-        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-      } beats ${playerSelection}!`
-    );
-    return computerScore++;
-  } else if (
-    (computerSelection === 'rock' && playerSelection === 'rock') ||
-    (computerSelection === 'paper' && playerSelection === 'paper') ||
-    (computerSelection === 'scissors' && playerSelection === 'scissors')
-  ) {
-    console.log(
-      `Draw! ${
-        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-      } beats ${playerSelection}`
-    );
-  } else if (
-    (computerSelection === 'rock' && playerSelection === 'paper') ||
-    (computerSelection === 'paper' && playerSelection === 'scissors') ||
-    (computerSelection === 'scissors' && playerSelection === 'rock')
-  ) {
-    console.log(
-      `You win! ${
-        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-      } beats ${computerSelection}!`
-    );
-    return playerScore++;
   }
 }
 
@@ -70,10 +27,121 @@ function checkWinner() {
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-  }
+let win = document.querySelector('.win');
+let draw = document.querySelector('.draw');
+let lose = document.querySelector('.lose');
 
-  checkWinner();
+let computer = document.querySelector('.computer');
+let player = document.querySelector('.player');
+
+let rock = document.querySelector('.rock');
+let scissors = document.querySelector('.scissors');
+let paper = document.querySelector('.paper');
+let reset = document.querySelector('.reset');
+
+let computerScore = document.querySelector('.computerScore');
+let playerScore = document.querySelector('.playerScore');
+
+let computerPoints = Number(computerScore.innerText);
+let playerPoints = Number(playerScore.innerText);
+
+function resetGame() {
+  computerScore.innerText = 0;
+  playerScore.innerText = 0;
+  computerPoints = 0;
+  playerPoints = 0;
+  computer.textContent = ' ';
+  player.textContent = ' ';
+  lose.classList.add('hide');
+  win.classList.add('hide');
+  draw.classList.add('hide');
 }
+
+rock.addEventListener(
+  'click',
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = rock.innerText.toLowerCase();
+    player.innerText = `Player chose ${playerSelection}`;
+
+    computerSelection = computerPlay();
+    computer.innerText = `Computer chose ${computerSelection}`;
+
+    if (computerSelection === 'paper') {
+      lose.classList.remove('hide');
+      win.classList.add('hide');
+      draw.classList.add('hide');
+
+      computerPoints += 1;
+      computerScore.innerHTML = computerPoints;
+    } else if (computerSelection === 'scissors') {
+      win.classList.remove('hide');
+      lose.classList.add('hide');
+      draw.classList.add('hide');
+      playerPoints += 1;
+      playerScore.innerHTML = playerPoints;
+    } else {
+      draw.classList.remove('hide');
+      lose.classList.add('hide');
+      win.classList.add('hide');
+    }
+  }
+);
+
+scissors.addEventListener(
+  'click',
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = scissors.innerText.toLowerCase();
+    player.innerText = `Player chose ${playerSelection}`;
+
+    computerSelection = computerPlay();
+    computer.innerText = `Computer chose ${computerSelection}`;
+    if (computerSelection === 'rock') {
+      lose.classList.remove('hide');
+      win.classList.add('hide');
+      draw.classList.add('hide');
+      computerPoints += 1;
+      computerScore.innerHTML = computerPoints;
+    } else if (computerSelection === 'paper') {
+      win.classList.remove('hide');
+      lose.classList.add('hide');
+      draw.classList.add('hide');
+      playerPoints += 1;
+      playerScore.innerHTML = playerPoints;
+    } else {
+      draw.classList.remove('hide');
+      lose.classList.add('hide');
+      win.classList.add('hide');
+    }
+  }
+);
+
+paper.addEventListener(
+  'click',
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = paper.innerText.toLowerCase();
+    player.innerText = `Player chose ${playerSelection}`;
+
+    computerSelection = computerPlay();
+    computer.innerText = `Computer chose ${computerSelection}`;
+
+    if (computerSelection === 'scissors') {
+      lose.classList.remove('hide');
+      win.classList.add('hide');
+      draw.classList.add('hide');
+      computerPoints += 1;
+      computerScore.innerHTML = computerPoints;
+    } else if (computerSelection === 'rock') {
+      win.classList.remove('hide');
+      lose.classList.add('hide');
+      draw.classList.add('hide');
+      playerPoints += 1;
+      playerScore.innerHTML = playerPoints;
+    } else {
+      draw.classList.remove('hide');
+      lose.classList.add('hide');
+      win.classList.add('hide');
+    }
+  }
+);
+
+reset.addEventListener('click', resetGame);
